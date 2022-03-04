7b1b5d419f71dfeca2efe70d8459a32d12281d34
Siddhant <santoshshrivastava39@gmail.com>
2022-03-05 00:32:24 +0530

    CalculatePowerof2

---
+++ powerof2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Basicfunctionalprograms
+{
+    public class powerof2
+    {
+        public static void Printpower()
+        {
+            Console.WriteLine("Please enter the value of n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int power = 1;
+            if (n <= 31)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    power = power * 2;
+                }
+
+                Console.WriteLine("The power of 2= " + power);
+
+
+
+
+
+            }
+        }
+    }
+
+
+}
+
