eb3738b842a1db4b5b95f03fdc900eb0a6870640
Siddhant <santoshshrivastava39@gmail.com>
2022-03-05 00:30:47 +0530

    Update the main method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Basicfunctionalprograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            powerof2.Printpower();
+        }
+    }
+}
+
+
