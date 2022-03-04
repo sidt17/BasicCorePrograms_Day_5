047f8d09e6519ba3b369460ffb3a3bcbcd504442
Siddhant <santoshshrivastava39@gmail.com>
2022-03-05 00:09:39 +0530

    Update the main method

---
+++ LeapYear.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Basicfunctionalprograms
+{
+    public class LeapYear
+    {
+        public static void Calculateleapyear() 
+        {
+
+            Console.WriteLine("Enter the year :");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("It is a leap year");
+
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(" It is not a leap year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine("It is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("It is not a leap year");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
