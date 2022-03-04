cbb5d74b3bdfe460759fe953f5069216f5bc499e
Siddhant <santoshshrivastava39@gmail.com>
2022-03-05 00:40:48 +0530

    Calculate Harmonic Numbers

---
+++ Harmonicnumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Basicfunctionalprograms
+{
+    public class Harmonicnumber
+    {
+
+        public static void CalculateHarmonicno()
+        {
+            Console.WriteLine(" Please enter the value of n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i;
+            double s = 0;
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/" + i);
+                s += 1 / (float)i;
+            }
+
+            Console.WriteLine("harmonic mean upto " + n + " = " + s);
+
+        }
+    }
+
+
+
+}
+
