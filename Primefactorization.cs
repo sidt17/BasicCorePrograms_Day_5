7ce848f8d09ce530e5b8b6f066fd87424bf0a79e
Siddhant <santoshshrivastava39@gmail.com>
2022-03-05 00:49:14 +0530

    Compute Prime Factorization

---
+++ Primefactorization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicfuctionalprograms
+{
+    public class Primefactorization
+    {
+        public static void Factors()
+        {
+            int n;
+
+            Console.WriteLine("Please enter the value of n :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            double fact = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("factorial of {0}={1}", n, fact);
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
